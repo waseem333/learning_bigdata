2b7adbb8bcf81b58109c5cc126349b6691680c4c
waseem333 <34704574+waseem333@users.noreply.github.com>
2017-12-27 12:16:34 -0500

    Add files via upload

---
+++ scala/learning_scala/scala_learning (1).scala
@@ -0,0 +1,74 @@
+// Databricks notebook source
+val a=10
+
+// COMMAND ----------
+
+val b = 1.2
+
+// COMMAND ----------
+
+val c =1.3f
+
+// COMMAND ----------
+
+val d = "waseem"
+
+// COMMAND ----------
+
+val e = true
+
+// COMMAND ----------
+
+val b : Int =10
+
+// COMMAND ----------
+
+val f : Double = 10.2
+
+// COMMAND ----------
+
+val g : String = "waseem"
+
+// COMMAND ----------
+
+val h : Boolean = true
+
+// COMMAND ----------
+
+var a =10
+
+// COMMAND ----------
+
+a = 20
+
+// COMMAND ----------
+
+a= 30
+
+// COMMAND ----------
+
+var b = 30
+
+// COMMAND ----------
+
+b = 40
+
+// COMMAND ----------
+
+val a = 30
+
+// COMMAND ----------
+
+a= 40
+
+// COMMAND ----------
+
+var z = 20.6
+
+// COMMAND ----------
+
+z = 30.2
+
+// COMMAND ----------
+
+
